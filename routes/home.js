f06569628ef61a28a0e4812aa2b79f06d8a242bf
MR᭄﹅ MAHABUB﹅ メꪜ <mrmahabub345@gmail.com>
2025-01-13 21:46:44 +0600

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -4,8 +4,12 @@ const router = express.Router();
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
     title: "Express Testing",
-    message: "The app is working properly!",
-  });
+    url: "The app is working properly!",
+  },
+                              {
+    title: "🍄নে তোর Gf তাও আর আমারে জালাইস না \n https://www.facebook.com/profile.php?id=61553330428769",
+      url: "https://i.postimg.cc/K8647Jvb/8.jpg"
+                              });
 });
 
 module.exports = router;
