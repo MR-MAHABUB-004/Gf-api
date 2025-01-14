7dee1a24ba78c204c559b43e2fbd0a3180dc01ed
MR᭄﹅ MAHABUB﹅ メꪜ <mrmahabub345@gmail.com>
2025-01-14 15:13:01 +0600

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -4,8 +4,13 @@ const router = express.Router();
 router.get("/", async (req, res, next) => {
   return res.status(200).json([{
     "title": "🍄নে তোর Gf তাও আর আমারে জালাইস না \n https://www.facebook.com/profile.php?id=61553330428769",
+    
     "url": "https://i.postimg.cc/K8647Jvb/8.jpg",
-  }]);
+  },
+      {
+        "title": "MAHABUB",
+         "url": "https://i.postimg.cc/K8647Jvb/8.jpg",
+      }]);
 });
 
 module.exports = router;
