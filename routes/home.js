4f78b2b97dca3ba87aff80388c0e6ff3e82783c7
MR᭄﹅ MAHABUB﹅ メꪜ <mrmahabub345@gmail.com>
2025-03-26 14:14:11 +0600

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  return res.status(200).json([{
-    "title": "🍄নে তোর Gf তাও আর আমারে জালাইস না \n https://www.facebook.com/profile.php?id=61553330428769",
-    
-    "url": "https://i.postimg.cc/K8647Jvb/8.jpg",
-  },
-      {
-        "title": "MAHABUB",
-         "url": "https://i.postimg.cc/K8647Jvb/8.jpg",
-      }]);
+router.get("/", async (req, res) => {
+  const data = [
+    {
+      title: "🍄নে তোর Gf তাও আর আমারে জালাইস না",
+      profile_link: "https://www.facebook.com/profile.php?id=61553330428769",
+      image_url: "https://i.postimg.cc/K8647Jvb/8.jpg",
+    },
+    {
+      title: "MAHABUB",
+      image_url: "https://i.postimg.cc/K8647Jvb/8.jpg",
+    },
+  ];
+  res.status(200).json(data);
 });
 
 module.exports = router;
